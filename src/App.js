a1402828f07e735820d3a3ee9753f1403cc1f5fc
Maria <mariale171001@gmail.com>
2023-03-13 22:42:55 +0100

    define states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
+// import styles
+import "./styles/app.scss";
+// import componets
+import Player from "./components/Player";
+import Song from "./components/Song";
+import data from "./util";
+
 
 function App() {
+  // state 
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song currentSong={currentSong}/>
+      <Player />
     </div>
   );
 }
